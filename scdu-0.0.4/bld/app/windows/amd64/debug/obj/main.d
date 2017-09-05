94dbd14c532241358fe9a6ba9901d64b5ce3b643
RVJ Callanan <roger@rvjc.com>
2017-09-05 18:28:31 +0100

    Add files via upload

---
+++ scdu-0.0.4/bld/app/windows/amd64/debug/obj/main.d
@@ -0,0 +1,8 @@
+bld/app/windows/amd64/debug/obj/main.o: src/app/main.cpp \
+ src/app/../core/core.h src/app/../core/platform.h \
+ src/app/../core/error.h src/app/../core/mem.h src/app/../core/math.h \
+ src/app/../core/str.h src/app/../core/type.h src/app/../core/quantity.h \
+ src/app/../core/var.h src/app/../core/parse.h src/app/../core/format.h \
+ src/app/../core/value.h src/app/../core/cmd.h src/app/../core/channels.h \
+ src/app/copy.h src/app/help.h src/app/info.h src/app/show.h \
+ src/app/view.h
---
+++ scdu-0.0.4/bld/app/windows/amd64/debug/obj/show.d
@@ -0,0 +1,7 @@
+bld/app/windows/amd64/debug/obj/show.o: src/app/show.cpp \
+ src/app/../core/core.h src/app/../core/platform.h \
+ src/app/../core/error.h src/app/../core/mem.h src/app/../core/math.h \
+ src/app/../core/str.h src/app/../core/type.h src/app/../core/quantity.h \
+ src/app/../core/var.h src/app/../core/parse.h src/app/../core/format.h \
+ src/app/../core/value.h src/app/../core/cmd.h src/app/../core/channels.h \
+ src/app/show.h
