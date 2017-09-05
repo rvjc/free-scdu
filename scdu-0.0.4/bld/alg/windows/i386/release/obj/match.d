3f116c649ba219ff92b1067713c041f3c1104a04
RVJ Callanan <roger@rvjc.com>
2017-09-05 18:25:08 +0100

    Add files via upload

---
+++ scdu-0.0.4/bld/alg/windows/i386/release/obj/match.d
@@ -0,0 +1,6 @@
+bld/alg/windows/i386/release/obj/match.o: src/alg/match.cpp \
+ src/alg/../core/core.h src/alg/../core/platform.h \
+ src/alg/../core/error.h src/alg/../core/mem.h src/alg/../core/math.h \
+ src/alg/../core/str.h src/alg/../core/type.h src/alg/../core/quantity.h \
+ src/alg/../core/var.h src/alg/../core/parse.h src/alg/../core/format.h \
+ src/alg/../core/value.h src/alg/../core/cmd.h src/alg/../core/channels.h
