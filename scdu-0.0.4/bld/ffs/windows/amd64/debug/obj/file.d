8bbef117636a2f1fd3d56b737bdfc40a13306e2d
RVJ Callanan <roger@rvjc.com>
2017-09-05 18:30:15 +0100

    Add files via upload

---
+++ scdu-0.0.4/bld/ffs/windows/amd64/debug/obj/file.d
@@ -0,0 +1,7 @@
+bld/ffs/windows/amd64/debug/obj/file.o: src/ffs/file.cpp \
+ src/ffs/../core/core.h src/ffs/../core/platform.h \
+ src/ffs/../core/error.h src/ffs/../core/mem.h src/ffs/../core/math.h \
+ src/ffs/../core/str.h src/ffs/../core/type.h src/ffs/../core/quantity.h \
+ src/ffs/../core/var.h src/ffs/../core/parse.h src/ffs/../core/format.h \
+ src/ffs/../core/value.h src/ffs/../core/cmd.h src/ffs/../core/channels.h \
+ src/ffs/file.h
