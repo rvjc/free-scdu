41cfdd99a527d9cfd25ed7e6ca22d4c4961479d5
RVJ Callanan <roger@rvjc.com>
2017-09-05 18:35:59 +0100

    Add files via upload

---
+++ scdu-0.0.4/bld/core/windows/i386/release/obj/math.d
@@ -0,0 +1,5 @@
+bld/core/windows/i386/release/obj/math.o: src/core/math.cpp \
+ src/core/core.h src/core/platform.h src/core/error.h src/core/mem.h \
+ src/core/math.h src/core/str.h src/core/type.h src/core/quantity.h \
+ src/core/var.h src/core/parse.h src/core/format.h src/core/value.h \
+ src/core/cmd.h src/core/channels.h
